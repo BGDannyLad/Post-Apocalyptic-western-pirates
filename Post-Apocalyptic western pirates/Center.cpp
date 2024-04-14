34f5dd84d9baafa59baf468c3b7b64a5567ecfa8
BGDannyLad <113034371+BGDannyLad@users.noreply.github.com>
2024-04-14 15:53:07 -0400

    added test in center

--- Post-Apocalyptic western pirates/Center.cpp
+++ Post-Apocalyptic western pirates/Center.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include "Items.h"
+#include "Inventory.h"
 using namespace std;
 
 int main() {
-	
+	inventory test;
+	test.display();
+	test.addItem(1, 1);
+	test.display();
 }
