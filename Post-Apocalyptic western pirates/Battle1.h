b2628470daa719cde6fc0fb70da90c7d290af026
David Born <dborn001@student.franciscan.edu>
2024-04-14 15:46:01 -0400

    Battle and Enemy1

---
+++ Post-Apocalyptic western pirates/Battle1.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Enemy1.h"
+#include "Items.h"
+#include "Inventory.h"
+#include <iostream>
+using namespace std; 
+
+
+int enemyattack() {
+
+}
+
+int enemyheal(){
+	if (health < 10) {
+		randi
+	}
+}
+
+int enemyflee(){
+
+}
+
+int Fighting() {
+
+}
